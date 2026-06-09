416cbfecea5e5fe30bf5253a8c0c305dceb61347
rostiaardappel-eng <rosti.aardappel@gmail.com>
2026-06-09 16:40:23 +0200

    Update streamlit_app.py
    error handles

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if ingredients_list:
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smootiefroot_response.json(), use_container_width = True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
 
 
