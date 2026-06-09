30f318493089d7a083656762a7d6c11e9933dc6d
rostiaardappel-eng <rosti.aardappel@gmail.com>
2026-06-09 16:36:14 +0200

    Update streamlit_app.py
    changed the api link

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
 
 # New sections to display smoothiefroot nutrion information
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
 
 
