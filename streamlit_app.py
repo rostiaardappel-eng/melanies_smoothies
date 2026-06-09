6e266f29297de325c76d59d7734fe20801d0b505
rostiaardappel-eng <rosti.aardappel@gmail.com>
2026-06-09 17:44:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 pd_df=my_dataframe.to_pandas()
 # st.dataframe(pd_df)
 # st.stop()
+st.write(pd_df.columns)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
